5dd7218640e9e67c7624af037a1043f6214293d6
Otto Seiskari <otto.seiskari@indooratlas.com>
2018-03-20 11:40:47 +0200

    asset tracker endpoint, first version

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,29 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const cors = require('cors');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase); // firebase DB
+const app = express();
+
+// Automatically allow cross-origin requests
+//app.use(cors());
+
+app.put('/:agentId', (req, res) => {
+  const agentId = req.params.agentId;
+
+  const msg = req.body;
+
+  return admin.database()
+    .ref(`agent_messages/${agentId}`)
+    .set(msg)
+    .then((snapshot) => {
+      return res.status(200).send('ok');
+    })
+    .catch((error) => {
+      res.status(500).send(err);
+    });
+});
+
+// Expose Express API as a single Cloud Function:
+exports.agent = functions.https.onRequest(app);
