ec5395250d252f3657e2622ff5af22cd673758b7
Otto Seiskari <otto.seiskari@indooratlas.com>
2018-03-20 18:01:08 +0200

    different asset colors, looks messy

--- public/main.js
+++ public/main.js
@@ -20,10 +20,17 @@ function getFirst(assets, path) {
 
 function AssetView(map) {
   const circles = {};
+  const markers = {};
+
   const featureGroup = L.featureGroup().addTo(map);
 
-  // http://leafletjs.com/reference.html#circle
-  var accuracyCircleOptions =
+  const ASSET_COLORS = [
+    [0, 0, 255],
+    [255, 0, 0],
+    [128, 255, 0],
+    [0, 128, 255],
+    [255, 255, 0]
+  ];
 
   this.update = (agentId, pos) => {
     const coords = pos.location.coordinates;
@@ -31,21 +38,29 @@ function AssetView(map) {
     const latLon = [coords.lat, coords.lon];
 
     let circle = circles[agentId];
+
     if (!circle) {
       console.log(`rendering ${agentId}`);
+      const col = ASSET_COLORS[_.keys(circles).length % ASSET_COLORS.length];
+      const toRgb = col => `rgb(${col[0]}, ${col[1]}, ${col[2]})`;
 
       circle = L.circle(latLon, radius, {
-        color: '#000080', // Stroke color
-        opacity: 0.7, // Stroke opacity
+        color: toRgb(col.map(c => Math.round(c/2))), // Stroke color
+        opacity: 0.3, // Stroke opacity
         weight: 1, // Stroke weight
-        fillColor: '#0000ff', // Fill color
-        fillOpacity: 0.2 // Fill opacity
+        fillColor: toRgb(col),
+        fillOpacity: 0.02
       })
       .addTo(featureGroup);
+
+      markers[agentId] = L.marker(latLon, {title: agentId, alt: agentId})
+      .addTo(featureGroup);
+
       circles[agentId] = circle;
     } else {
       circle.setLatLng(latLon);
       circle.setRadius(radius);
+      markers[agentId].setLatLng(latLon);
     }
   }
 }
@@ -67,6 +82,7 @@ function runApp(map, user) {
   function drawAssets(assets) {
     if (!floorPlanId) {
       floorPlanId = getFirst(assets, 'context.indooratlas.floorPlanId');
+      console.log(`showing floor plan ${floorPlanId}`);
       floorPlanManager.onEnterFloorPlan(floorPlanId);
     }
     if (!centerCoords) {
@@ -75,7 +91,9 @@ function runApp(map, user) {
        map.setView(L.latLng(centerCoords.lat, centerCoords.lon), /*ZOOM*/ 20);
       }
     }
-    _.forEach(assets, (pos, agentId) => {
+
+    _.keys(assets).sort().forEach(agentId => {
+      const pos = assets[agentId];
       if (_.get(pos, 'location.coordinates')) {
         assetView.update(agentId, pos);
       }
